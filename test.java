3d8f49d9c37af8cf79a91bc68950349053a922d2
maihahoangthai <maihahoangthai@gmail.com>
2023-11-22 13:50:01 +0700

    second update to test.java

--- test.java
+++ test.java
@@ -1,7 +1,15 @@
+import java.util.Scanner; /* Thư viện để lấy input từ User */
+
 public class test{
     public static void main(String[] args){
-        System.out.print("Hello World!"); // print khi output sẽ không xuống dòng.
-        System.out.println("Fuck1"); // println mỗi output là 1 dòng mới.
+        System.out.print("Hello World!"); // output của "print" không xuống dòng.
+        System.out.println("Fuck1"); // mỗi output của "println" là 1 dòng mới.
         System.out.println("Fuck2");
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String name = myScanner.nextLine();
+        System.err.print("Your name is " + name + ", right?\n");
+        myScanner.close();
     }
 }
